c8739490c45684da1f348d98ea081107214bd509
nishanth-1431 <hinishanth777@gmail.com>
2026-06-11 22:09:01 +0530

    added print.java

---
+++ 01-basics/print.java
@@ -0,0 +1,6 @@
+public class print {
+    public static void main(String[] args) {
+        System.out.print("prints in same lines until \n used " );
+        System.out.println("prints in new line");
+    }
+}
