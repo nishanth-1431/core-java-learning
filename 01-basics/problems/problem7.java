7b06719f90ab59af76b00329c7966fd82bd7098a
nishanth-1431 <hinishanth777@gmail.com>
2026-06-13 23:55:25 +0530

    solved problems

---
+++ 01-basics/problems/problem7.java
@@ -0,0 +1,11 @@
+//	Write a Java program to swap two numbers.
+public class problem7 {
+    public static void main(String[] args){
+       int a=10;
+         int b=20;
+            int temp = a;
+            a = b;
+            b = temp;
+            System.out.println("After swapping: a = " + a + ", b = " + b);
+    }    
+}
