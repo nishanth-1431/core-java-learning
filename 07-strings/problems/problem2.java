5c0cbf05831d789870c4b4eb63119ed3ebd225c8
nishanth-1431 <hinishanth777@gmail.com>
2026-06-18 00:15:32 +0530

    solved problem

--- 07-strings/problems/problem2.java
+++ 07-strings/problems/problem2.java
@@ -5,11 +5,22 @@ import java.util.*;
 public class problem2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a String :");
+        System.out.print("Enter a String  :");
         String str = sc.nextLine();
         int length = str.length();
+        System.out.println("The Length of the String      : "+length);
         char first = str.charAt(0);
+        System.out.println("The first letter of String    : "+first);
+        if(length % 2==0){
+            char middle1 = str.charAt(length/2 - 1);
+            char middle2 = str.charAt(length/2);
+        System.out.println(" The middle letters of String : "+ middle1 +" and "+ middle2);
+        }
+        else{
+            char middle = str.charAt(length/2);
+        System.out.println("The middle letter of String   : "+ middle);
+        }
         char last = str.charAt(length-1);
-
+        System.out.println("The last letter of String     : "+first);
     }
 }
