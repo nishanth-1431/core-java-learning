a801d59c7bb26c32c098c155c1eff55143e34219
nishanth-1431 <hinishanth777@gmail.com>
2026-06-19 22:25:08 +0530

    learnt for loop

---
+++ 03-loops/forloop.java
@@ -0,0 +1,15 @@
+/*  for loop :
+   - The for loop is used when we know the number of iterations(how many times to repeat the task)
+   - The for statement includes the initialization, condition, and increment/decrement in one line. 
+    Syntax:
+    for(initialization ; condition ; increment (++)\ decrement(--)){
+         //statement or code to be executed
+    }
+ */
+public class forloop {
+    public static void main(String[] args) {
+        for(int i=0 ; i < 11 ;i++){ // this prints 0 to 10 
+            System.out.println(i);
+        }
+    }
+}
---
+++ 03-loops/introduction.java
@@ -0,0 +1,50 @@
+/*
+Introduction to Loops
+
+Loops are used to execute a block of code repeatedly until a specified condition becomes false.
+They help avoid writing the same code multiple times and make programs more efficient.
+
+Types of loops in Java:
+
+1. for loop
+   - Used when the number of iterations is known.
+   Syntax:
+   for(initialization; condition; update) {
+       // statements
+   }
+
+2. while loop
+   - Used when the number of iterations is unknown.
+   Syntax:
+   while(condition) {
+       // statements
+   }
+
+3. do-while loop
+   - Executes the loop body at least once before checking the condition.
+   Syntax:
+   do {
+       // statements
+   } while(condition);
+
+4. Enhanced for loop
+   - Used to traverse arrays and collections.
+   Syntax:
+   for(dataType variable : array) {
+       // statements
+   }
+
+Loop Control Statements:
+
+1. break
+   - Terminates the loop immediately.
+
+2. continue
+   - Skips the current iteration and proceeds to the next iteration.
+
+3. nested loops
+   - A loop inside another loop.
+*/
+public class introduction {
+    // examples are executed in upcomming files and problems are found in \03-loops\problems
+}
