ae1049ee31150988e4d5a6071bc6f2ce92ab9ed0
nishanth-1431 <hinishanth777@gmail.com>
2026-06-21 09:14:01 +0530

    solved problem

---
+++ 03-loops/problems/problem1.java
@@ -0,0 +1,9 @@
+/* 1.	Print numbers from 1 to 100. 
+ */
+public class problem1 {
+    public static void main(String[] args) {
+        for(int i = 1 ; i<=100 ; i++){
+            System.out.println("number "+i);
+        }
+    }
+}
