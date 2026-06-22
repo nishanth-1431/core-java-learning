976e39ecfba53161de69d040347de16318b9f95e
nishanth-1431 <hinishanth777@gmail.com>
2026-06-22 22:49:54 +0530

    learnt array basics

--- 04-arrays/ArrayBasics.java
+++ 04-arrays/ArrayBasics.java
@@ -1,56 +1,16 @@
-import java.util.Scanner;
-
+/* What is Array?
+        - An array is a collection of elements of the same data type stored in contiguous memory locations. 
+        - It allows multiple values to be stored under a single name and accessed using an index. 
+        - Java arrays can hold both primitive types (like int, char, boolean, etc.) and objects (like String, Integer, etc.)
+        Syntax:
+        dataType[] arrayName; (or)
+        dataType arrayName[];
+   */
 public class ArrayBasics {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        // ========== EXAMPLE 1: Creating and printing an array ==========
-        int[] marks = {90, 85, 78, 92, 88};
-
-        System.out.println("===== Student Marks =====");
-        for (int i = 0; i < marks.length; i++) {
-            System.out.println("Student " + (i + 1) + ": " + marks[i]);
-        }
-
-        // ========== EXAMPLE 2: Taking array input from user ==========
-        System.out.print("\nHow many numbers do you want to enter? ");
-        int size = sc.nextInt();
-        int[] numbers = new int[size];
-
-        System.out.println("Enter " + size + " numbers:");
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.print("Number " + (i + 1) + ": ");
-            numbers[i] = sc.nextInt();
-        }
-
-        // ========== EXAMPLE 3: Finding the sum and average ==========
-        int sum = 0;
-        for (int num : numbers) {
-            sum += num;
-        }
-        double average = (double) sum / numbers.length;
-
-        System.out.println("\nSum = " + sum);
-        System.out.println("Average = " + average);
-
-        // ========== EXAMPLE 4: Finding the largest element ==========
-        int max = numbers[0];  // Assume first element is the largest
-        for (int i = 1; i < numbers.length; i++) {
-            if (numbers[i] > max) {
-                max = numbers[i];
-            }
-        }
-        System.out.println("Largest number = " + max);
-
-        // ========== EXAMPLE 5: Finding the smallest element ==========
-        int min = numbers[0];
-        for (int i = 1; i < numbers.length; i++) {
-            if (numbers[i] < min) {
-                min = numbers[i];
-            }
-        }
-        System.out.println("Smallest number = " + min);
-
-        sc.close();
+        // primitive array - stores int, char, boolean and traversed using loop
+        int[] arr =  {1,2,3,4};  // creating array (primitive array)
+        // non primitive array - The non-primitive array stores String objects and is printed in the same way using its length property.
+        String[] name = {"elon" , "jeff"}; // creating array (non primitive array)
     }
 }
