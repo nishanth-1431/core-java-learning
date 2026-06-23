08962e9bac9b937cbf039273c18339ce2dce4dea
nishanth-1431 <hinishanth777@gmail.com>
2026-06-23 23:50:10 +0530

    learned array basics

--- 04-arrays/ArrayBasics.java
+++ 04-arrays/ArrayBasics.java
@@ -18,13 +18,12 @@ public class ArrayBasics {
         //  Initialization of arrays :
         // 1. Array Literal Initialization
         int[] arr1 = {1, 2, 3, 4};
-        
+
         // 2.Array Creation with new
         int[] arr2 = new int[4];
         arr2[0] = 1;
         arr2[1] = 2;
         arr2[2] = 3;
         arr2[3] = 4;
-
     }
 }
