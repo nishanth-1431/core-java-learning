0b7a074c31c738c7ea078fcac1ec28534315660a
nishanth-1431 <hinishanth777@gmail.com>
2026-06-24 13:46:05 +0530

    solved problem to create and print elements

---
+++ 04-arrays/problems/problem1.java
@@ -0,0 +1,11 @@
+/*  1. Create an array of 5 integers and print all elements.
+   Topics: Array declaration, creation, initialization, for loop
+ */
+public class problem1 {
+    public static void main(String[] args) {
+        int[] arr = {0,1,2,3,4};
+        for(int i = 0 ; i<arr.length ; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
