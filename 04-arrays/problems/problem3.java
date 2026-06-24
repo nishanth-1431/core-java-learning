37d44f8b4448ba0dd38c33ab2fd52e09d42035f6
nishanth-1431 <hinishanth777@gmail.com>
2026-06-24 14:04:24 +0530

    solved problem to find length of array

---
+++ 04-arrays/problems/problem3.java
@@ -0,0 +1,9 @@
+/* 3. Find the length of an array.
+   Topics: length property
+ */
+public class problem3 {
+    public static void main(String[] args) {
+        int[] arr = {10,20,30,40,50};
+        System.out.println("the length of the array is : "+arr.length);
+    }
+}
