50650ee6ac1ccf7a5b9265f9d149e5a920a3b821
nishanth-1431 <hinishanth777@gmail.com>
2026-06-26 21:42:18 +0530

    added oops practice questions

---
+++ 08-oop/problems/problem1.java
@@ -0,0 +1,19 @@
+/* 1. Student Details
+Create a Student class.
+Fields:
+- name
+- age
+- department
+Method:
+- displayDetails()
+Create one object in main() and display the student's details.
+Topics Used:
+- Class
+- Object
+- Fields
+- Methods
+ */
+
+public class problem1 {
+    
+}
