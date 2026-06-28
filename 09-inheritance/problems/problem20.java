0151059bca8b245fea975e02ba15e43108f83b46
nishanth-1431 <hinishanth777@gmail.com>
2026-06-28 22:19:19 +0530

    solved hierarchical inheritance

---
+++ 09-inheritance/problems/problem20.java
@@ -0,0 +1,23 @@
+/* 20. Mini E-Commerce Website (Challenge)
+Topics Used: Hierarchical Inheritance
+User
+  Fields:    username, email
+  Methods:   login()
+Customer
+  Fields:    address
+  Methods:   placeOrder()
+Seller
+  Fields:    shopName
+  Methods:   addProduct()
+Admin
+  Fields:    adminCode
+  Methods:   removeProduct()
+Note: Create one object of each class and demonstrate inheritance.
+ */
+class User{
+    String username;
+    String email;
+}
+public class problem20 {
+     
+}
