b4c38600506ebededd8bad5d0ea0f0b475a1abdf
nishanth-1431 <hinishanth777@gmail.com>
2026-07-01 23:57:54 +0530

    solved problem13 on 2d arrays

---
+++ 04-arrays/problems/problem13.java
@@ -0,0 +1,22 @@
+/* 13. Create a 3×3 matrix and print all elements.
+   Topics: 2D array declaration, nested loops
+*/
+public class problem13 {
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        System.out.println("3 x 3 Matrix:");
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
