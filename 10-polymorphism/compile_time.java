56fff96e98341fd1228a6895eeb66abae1432ba1
nishanth-1431 <hinishanth777@gmail.com>
2026-07-05 21:59:35 +0530

    learnt compile time polymorphism with method overloading

---
+++ 10-polymorphism/compile_time.java
@@ -0,0 +1,59 @@
+/* 1. Compile-time Polymorphism :
+      - Definition
+        When the method call is decided during compilation, it is called Compile-time Polymorphism.
+      - Achieved By
+        Method Overloading
+      - Key Point
+        Same method name but different parameters.
+    
+    What is Method Overloading?
+        - Definition
+            If multiple methods have:
+            Same method name Different parameters then it is called Method Overloading.
+
+        - Rules
+            Methods can differ by:
+                Number of parameters,
+                Type of parameters,
+                Order of parameters.
+
+
+ */
+class Calculator {
+
+    // This method is used when we pass 2 integer values
+    void add(int a, int b) {
+        System.out.println(a + b);
+    }
+
+    // This method is used when we pass 3 integer values
+    void add(int a, int b, int c) {
+        System.out.println(a + b + c);
+    }
+
+    // This method is used when we pass 2 decimal values
+    void add(double a, double b) {
+        System.out.println(a + b);
+    }
+}
+
+public class compile_time {
+
+    public static void main(String[] args) {
+
+        // Creating an object of Calculator
+        Calculator obj = new Calculator();
+
+        // Java sees 2 int values, so it calls add(int, int)
+        obj.add(10, 20);
+
+        // Java sees 3 int values, so it calls add(int, int, int)
+        obj.add(10, 20, 30);
+
+        // Java sees 2 double values, so it calls add(double, double)
+        obj.add(5.5, 2.5);
+    }
+}
+
+
+
