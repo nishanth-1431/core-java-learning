6b1db2e15820b512b71d6fd7f097e6c0c07424ce
nishanth-1431 <hinishanth777@gmail.com>
2026-07-08 15:48:09 +0530

    solved problem 5

---
+++ 10-polymorphism/problems/problem5.java
@@ -0,0 +1,28 @@
+class TemperatureConverter {
+
+    // Celsius to Fahrenheit
+    double convert(double celsius) {
+        return (celsius * 9 / 5) + 32;
+    }
+
+    // Fahrenheit to Celsius
+    double convert(double fahrenheit, boolean flag) {
+        return (fahrenheit - 32) * 5 / 9;
+    }
+
+    // Celsius to Kelvin
+    double convert(double celsius, int option) {
+        return celsius + 273.15;
+    }
+}
+
+public class problem5 {
+    public static void main(String[] args) {
+
+        TemperatureConverter tc = new TemperatureConverter();
+
+        System.out.println("25°C to Fahrenheit = " + tc.convert(25));
+        System.out.println("98.6°F to Celsius = " + tc.convert(98.6, true));
+        System.out.println("25°C to Kelvin = " + tc.convert(25, 1));
+    }
+}
