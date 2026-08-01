5fda358b2bd1e5e5cd2ab4bd8bde9de50fb874da
nishanth-1431 <hinishanth777@gmail.com>
2026-08-01 23:22:05 +0530

    Solved problem 6 from abstraction

--- 11-abstraction/problems/problem6.java
+++ 11-abstraction/problems/problem6.java
@@ -4,6 +4,19 @@ Create an interface Printable.
 •	Create a class Document that implements the interface. 
 •	Display a message using print(). 
  */
+interface Printable{
+    void print();
+} 
+class Document implements Printable{
+    @Override 
+    public 
+    void print(){
+        System.out.println("hello java");
+    }
+}
 public class problem6 {
-    
+    public static void main(String[] args) {
+        Printable p = new Document();
+        p.print();
+    }
 }
