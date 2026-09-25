89930eb410724535da6b9d903ef4fc64375be444
nishanth-1431 <hinishanth777@gmail.com>
2026-09-25 22:08:45 +0530

    solve(2D-arrays): Q14 - take user input and store elements in a 3x3 matrix

--- 04-arrays/problems/problem14.java
+++ 04-arrays/problems/problem14.java
@@ -1,31 +1,26 @@
-/* 14. Take elements from the user and store them in a 3×3 matrix.
+/* 14. Take elements from the user and store them in a 3x3 matrix.
    Topics: Scanner, nested loops
 */
 import java.util.Scanner;
-
 public class problem14 {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
         int[][] matrix = new int[3][3];
 
-        System.out.println("Enter 9 elements:");
-
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
+        System.out.println("Enter 9 elements for the 3x3 matrix:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print("Enter element [" + i + "][" + j + "]: ");
                 matrix[i][j] = sc.nextInt();
             }
         }
 
-        System.out.println("Matrix:");
-
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
+        System.out.println("\nYour 3x3 Matrix:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
                 System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
         }
-
-        sc.close();
     }
 }
