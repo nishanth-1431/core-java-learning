820ae1565f80dc1ba1dbcb94b14587bb2116639a
nishanth-1431 <hinishanth777@gmail.com>
2026-09-26 21:25:42 +0530

    solve(2D-arrays): Q16 - print matrix column by column using nested loops

--- 04-arrays/problems/problem16.java
+++ 04-arrays/problems/problem16.java
@@ -1,17 +1,22 @@
-/* 16. Find the sum of all even elements in an array.
-   Topics: loops, modulus operator (%), accumulator
+/* 16. Print the matrix column by column.
+   Topics: nested loops
 */
 public class problem16 {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
 
-        int sum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] % 2 == 0) {
-                sum = sum + arr[i];
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        System.out.println("Matrix column by column:");
+        for (int j = 0; j < matrix[0].length; j++) {
+            System.out.print("Column " + (j + 1) + ": ");
+            for (int i = 0; i < matrix.length; i++) {
+                System.out.print(matrix[i][j] + " ");
             }
+            System.out.println();
         }
-
-        System.out.println("Sum of all even elements: " + sum);
     }
 }
