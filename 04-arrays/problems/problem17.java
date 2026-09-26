0d07a7976fcc6b4610c969828981de4ec807195b
nishanth-1431 <hinishanth777@gmail.com>
2026-09-26 21:26:31 +0530

    solve(2D-arrays): Q17 - find sum of all elements in a matrix using accumulator

---
+++ 04-arrays/problems/problem17.java
@@ -0,0 +1,22 @@
+/* 17. Find the sum of all elements in a matrix.
+   Topics: nested loops, accumulator pattern
+*/
+public class problem17 {
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        int sum = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                sum += matrix[i][j];
+            }
+        }
+
+        System.out.println("Sum of all elements in the matrix: " + sum);
+    }
+}
