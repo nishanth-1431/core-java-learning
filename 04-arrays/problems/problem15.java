0fc336a0bd9f3d09578fe5534c2e39659d6a3a3e
nishanth-1431 <hinishanth777@gmail.com>
2026-09-26 21:24:55 +0530

    solve(2D-arrays): Q15 - print matrix row by row using nested loops

--- 04-arrays/problems/problem15.java
+++ 04-arrays/problems/problem15.java
@@ -1,22 +1,22 @@
-/* 15. Count how many times a given element occurs in an array.
-   Topics: loops, counter variable, equality check
+/* 15. Print the matrix row by row.
+   Topics: nested loops
 */
-import java.util.Scanner;
 public class problem15 {
     public static void main(String[] args) {
-        int[] arr = {3, 5, 3, 8, 3, 2, 5};
-        Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter the element to count: ");
-        int target = sc.nextInt();
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
 
-        int count = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == target) {
-                count++;
+        System.out.println("Matrix row by row:");
+        for (int i = 0; i < matrix.length; i++) {
+            System.out.print("Row " + (i + 1) + ": ");
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
             }
+            System.out.println();
         }
-
-        System.out.println(target + " occurs " + count + " time(s) in the array.");
     }
 }
